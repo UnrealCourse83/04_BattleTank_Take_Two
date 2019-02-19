fa4f8eab498335e757a56dbe15c61a2670758baf
Nic Basansky <n_basansky@hotmail.com>
2019-02-18 20:38:48 -0800

    At lecture 219. Will go until 225

---
+++ BattleTank/Source/BattleTank/Public/SpawnPoint.h
@@ -0,0 +1,34 @@
+// Copyright Nicholas Basansky 2019
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Public/Templates/Casts.h"
+#include "Components/ActorComponent.h"
+#include "Components/SceneComponent.h"
+#include "SpawnPoint.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class BATTLETANK_API USpawnPoint : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	USpawnPoint();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+private:
+	// Config
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+	TSubclassOf<AActor> SpawnClass;
+		
+};
